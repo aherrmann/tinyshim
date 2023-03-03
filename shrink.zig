09d7603b367173690d34a73691318380aacefd4a
Andreas Herrmann <andreash87@gmx.ch>
2023-03-03 08:50:16 +0100

    Factor out arg parsing

--- shrink.zig
+++ shrink.zig
@@ -4,40 +4,62 @@ const std = @import("std");
 const debug = std.debug;
 const io = std.io;
 
-pub fn main() !void {
-    // First we specify what parameters our program can take.
-    // We can use `parseParamsComptime` to parse a string into an array of `Param(Help)`
-    const params = comptime clap.parseParamsComptime(
-        \\-h, --help             Display this help and exit.
-        \\-n, --number <usize>   An option parameter, which takes a value.
-        \\-s, --string <str>...  An option parameter which can be specified multiple times.
-        \\<str>                  foo
-        \\<str>...               bar
+const Args = struct {
+    const exe_name = "shrink";
+    const params = clap.parseParamsComptime(
+        \\-h, --help   Display this help and exit.
+        \\<FILE>       The ELF file to shrink.
         \\
     );
-
-    // Initialize our diagnostics, which can be used for reporting useful errors.
-    // This is optional. You can also pass `.{}` to `clap.parse` if you don't
-    // care about the extra information `Diagnostics` provides.
-    var diag = clap.Diagnostic{};
-    var res = clap.parse(clap.Help, &params, clap.parsers.default, .{
-        .diagnostic = &diag,
-    }) catch |err| {
-        // Report useful error and exit
-        diag.report(io.getStdErr().writer(), err) catch {};
-        return err;
+    const parsers = .{
+        .FILE = clap.parsers.string,
     };
-    defer res.deinit();
 
-    if (res.args.help) {
-        try clap.usage(std.io.getStdErr().writer(), clap.Help, &params);
-        try std.io.getStdErr().writeAll("\n");
-        try clap.help(std.io.getStdErr().writer(), clap.Help, &params, .{});
+    diag: clap.Diagnostic,
+    res: clap.Result(clap.Help, &params, parsers),
+
+    elf_file: []const u8,
+
+    fn usage() !void {
+        try io.getStdErr().writer().print("{s} ", .{exe_name});
+        try clap.usage(io.getStdErr().writer(), clap.Help, &params);
+        try io.getStdErr().writeAll("\n\n");
     }
-    if (res.args.number) |n|
-        debug.print("--number = {}\n", .{n});
-    for (res.args.string) |s|
-        debug.print("--string = {s}\n", .{s});
-    for (res.positionals) |pos|
-        debug.print("{s}\n", .{pos});
+
+    pub fn parse() !?Args {
+        var diag = clap.Diagnostic{};
+        var res = clap.parse(clap.Help, &params, parsers, .{
+            .diagnostic = &diag,
+        }) catch |err| {
+            diag.report(io.getStdErr().writer(), err) catch {};
+            usage() catch {};
+            return err;
+        };
+        if (res.args.help) {
+            try usage();
+            try clap.help(io.getStdErr().writer(), clap.Help, &params, .{});
+            return null;
+        }
+        if (res.positionals.len < 1) {
+            try io.getStdErr().writeAll("Missing positional argument\n\n");
+            try usage();
+            return error.MissingArgument;
+        }
+        const elf_file = res.positionals[0];
+        return Args{
+            .diag = diag,
+            .res = res,
+            .elf_file = elf_file,
+        };
+    }
+
+    pub fn deinit(self: *Args) void {
+        self.res.deinit();
+    }
+};
+
+pub fn main() !void {
+    var args = (try Args.parse()) orelse return;
+    defer args.deinit();
+    debug.print("{s}\n", .{args.elf_file});
 }
