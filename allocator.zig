84f54034a101c91278c178afc1c46e5144fd271a
Andreas Herrmann <andreash87@gmx.ch>
2023-05-12 08:30:37 +0200

    add documentation comments

--- allocator.zig
+++ allocator.zig
@@ -2,18 +2,26 @@ const std = @import("std");
 const mem = std.mem;
 const os = std.os;
 
+/// A simple page allocator that allocates and frees memory
+/// from the operating system.
 const SimplePageAllocator = struct {
+    /// Allocates a memory region with the given `count` of bytes,
+    /// aligned to the system's page size, and returns a pointer to it.
+    /// Release the memory using `free`.
     pub fn alloc(count: usize) ![]align(mem.page_size) u8 {
         const size = mem.alignForward(count, mem.page_size);
         const ptr = try os.mmap(null, size, os.PROT.READ | os.PROT.WRITE, os.MAP.PRIVATE | os.MAP.ANONYMOUS, -1, 0);
         return ptr;
     }
 
+    /// Frees a memory region previously allocated with `alloc`.
     pub fn free(ptr: []align(mem.page_size) u8) void {
         os.munmap(ptr);
     }
 };
 
+/// A buffer that uses either stack or page allocation
+/// depending on the required size.
 fn StackOrPageBuffer(comptime stack_size: usize) type {
     return union(enum) {
         const Self = @This();
@@ -21,6 +29,7 @@ fn StackOrPageBuffer(comptime stack_size: usize) type {
         stack_buffer: [stack_size]u8,
         page_buffer: []u8,
 
+        /// Initializes the buffer with the given `required_size`.
         pub fn init(required_size: usize) !Self {
             if (required_size <= stack_size) {
                 return Self{ .stack_buffer = undefined };
@@ -29,6 +38,7 @@ fn StackOrPageBuffer(comptime stack_size: usize) type {
             }
         }
 
+        /// Deinitializes the buffer, freeing any allocated resources.
         pub fn deinit(self: *Self) void {
             switch (self.*) {
                 .stack_buffer => {},
@@ -36,6 +46,7 @@ fn StackOrPageBuffer(comptime stack_size: usize) type {
             }
         }
 
+        /// Returns the underlying buffer as a slice.
         pub fn get(self: *Self) []u8 {
             switch (self.*) {
                 .stack_buffer => |*slice| return slice,
@@ -83,10 +94,12 @@ test "StackOrPageBuffer page allocated" {
     try std.testing.expectEqualStrings(msg, buf.get()[0..msg.len]);
 }
 
+/// A simple bump allocator that allocates from a fixed-size buffer.
 const SimpleBumpAllocator = struct {
     buffer: []u8,
     cursor: usize,
 
+    /// Initializes the allocator with the given `buffer`.
     fn init(buffer: []u8) SimpleBumpAllocator {
         return SimpleBumpAllocator{
             .buffer = buffer,
@@ -94,6 +107,7 @@ const SimpleBumpAllocator = struct {
         };
     }
 
+    /// Allocates an array of `T` with `n` elements from the buffer.
     fn alloc(self: *SimpleBumpAllocator, comptime T: type, n: usize) ![]T {
         const align_offset = mem.alignForward(self.cursor, @alignOf(T));
         const required_size = n * @sizeOf(T);
